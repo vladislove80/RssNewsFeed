94456c1d114552068c961f830a0aaa8e12049d9e
Влад Ульяницкий <vladislove.n.u@gmail.com>
2016-09-23 13:36:56 +0300

    add NewsFeedItem

---
+++ app/src/main/java/jomedia/com/rssnewsfeed/NewsFeedItem.java
@@ -0,0 +1,42 @@
+package jomedia.com.rssnewsfeed;
+
+public class NewsFeedItem {
+    private String imageLink;
+    private String title;
+    private String date;
+    private String author;
+
+    public NewsFeedItem() {}
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
