02b4e75ec31969cd89c8eeb6da026581c3ea90dc
Влад Ульяницкий <vladislove.n.u@gmail.com>
2016-09-23 18:34:16 +0300

    add progressBar to webview activity, small fix to webview rotate

--- app/src/main/java/jomedia/com/rssnewsfeed/ui/activity/OpenItemActivity.java
+++ app/src/main/java/jomedia/com/rssnewsfeed/ui/activity/OpenItemActivity.java
@@ -3,26 +3,31 @@ package jomedia.com.rssnewsfeed.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import jomedia.com.rssnewsfeed.R;
 
 public class OpenItemActivity  extends AppCompatActivity {
     public static final String URL = "newsLink";
     private WebView mWebView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_open_item);
-
-        mWebView = (WebView)findViewById(R.id.web_view);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        if (getIntent() != null) {
-            mWebView.loadUrl(getIntent().getStringExtra(URL));
+            setContentView(R.layout.activity_open_item);
+            progressBar = (ProgressBar) findViewById(R.id.progressBar);
+            mWebView = (WebView)findViewById(R.id.web_view);
+            mWebView.getSettings().setJavaScriptEnabled(true);
+        if(savedInstanceState == null) {
+            if (getIntent() != null) {
+                mWebView.loadUrl(getIntent().getStringExtra(URL));
+            }
+            mWebView.setWebViewClient(new ItemWebViewClient(progressBar));
         }
-        mWebView.setWebViewClient(new ItemWebViewClient());
     }
 
     @Override
@@ -37,17 +42,42 @@ public class OpenItemActivity  extends AppCompatActivity {
                     }
                     return true;
             }
-
         }
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mWebView.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mWebView.restoreState(savedInstanceState);
+        progressBar.setVisibility(View.GONE);
+    }
+
     private class ItemWebViewClient extends WebViewClient {
+        private ProgressBar progressBar;
+
+        public ItemWebViewClient(ProgressBar progressBar){
+            this.progressBar=progressBar;
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url)
         {
             view.loadUrl(url);
             return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
         }
     }
 }
