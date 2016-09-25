60bd6d36c04218989df6eff1596c13c10bfc4b8e
Влад Ульяницкий <vladislove.n.u@gmail.com>
2016-09-25 22:28:21 +0300

    add ProgressBar and noData TextView to MainActivity

--- app/src/main/java/jomedia/com/rssnewsfeed/ui/activity/MainActivity.java
+++ app/src/main/java/jomedia/com/rssnewsfeed/ui/activity/MainActivity.java
@@ -5,17 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import jomedia.com.rssnewsfeed.data.model.NewsFeedItem;
 import jomedia.com.rssnewsfeed.R;
-import jomedia.com.rssnewsfeed.utils.Utils;
 import jomedia.com.rssnewsfeed.data.api.ApiManager;
 import jomedia.com.rssnewsfeed.data.model.Item;
+import jomedia.com.rssnewsfeed.data.model.NewsFeedItem;
 import jomedia.com.rssnewsfeed.data.model.RssModel;
 import jomedia.com.rssnewsfeed.ui.adapter.NewsFeedAdapter;
+import jomedia.com.rssnewsfeed.utils.Utils;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private NewsFeedAdapter mNewsFeedAdapter;
+    private TextView mTextViewNoData;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
         loadData();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.news_feed);
+        mTextViewNoData = (TextView) findViewById(R.id.no_data_textview);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(linearLayoutManager);
@@ -57,10 +65,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onDataError(Throwable throwable) {
+        progressBar.setVisibility(View.GONE);
+        mTextViewNoData.setVisibility(View.VISIBLE);
         Log.d(Utils.LOG, "onDataError " + throwable.toString());
     }
 
     private void onDataSuccess(RssModel rssModel) {
+        progressBar.setVisibility(View.GONE);
         if (rssModel != null) {
             mRssItemList = rssModel.getChannel().getItems();
             Log.d(Utils.LOG, "onDataSuccess ");
@@ -74,5 +85,4 @@ public class MainActivity extends AppCompatActivity {
         mSubscriptions.clear();
         super.onDestroy();
     }
-
 }
